90711a16197efbd5f803a7a2cf7a8d03cda731e6
Kavya-y <57026560+Kavya-y@users.noreply.github.com>
2019-10-26 10:53:32 +0530

    Update arr_pointers.c

--- HactoberFest_Programs/arr_pointers.c
+++ HactoberFest_Programs/arr_pointers.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main() {
 	int arr[]={1,2,3,4,5}; 
+	int *p=0;
 	//Declare a pointer ptr of type int
 	printf("\n Address of array = %p %p %p %p", arr, &arr[0], &arr,* p+2);
 	return 0;
