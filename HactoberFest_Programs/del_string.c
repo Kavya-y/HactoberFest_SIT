8f213e6701b32641c7321f4d7aa79cdbc497c5dc
Kavya-y <57026560+Kavya-y@users.noreply.github.com>
2019-10-26 11:18:44 +0530

    Update del_string.c

--- HactoberFest_Programs/del_string.c
+++ HactoberFest_Programs/del_string.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
 	char text[200],new_str[200];
 	int i=0,j=0,pos=0,num=0;
@@ -8,10 +9,11 @@ int main(){
 	scanf("%d",&pos);
 	printf("\nEnter the no. of characters to delete: ");
 	scanf("%d",&num);
-	while(text[i]!=NULL){
+	while(text[i]!='\0'){
 		/*
 		Write the code to delete num no. of characters from position pos in string text.
 		*/
+		if(i==pos) i=i+num;
 		new_str[j++]=text[i++];
 		
 	}
